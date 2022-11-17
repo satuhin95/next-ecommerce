251f768f1ca6c04b09e081dbc4929828fff33c55
satuhin95 <saifcm95@gmail.com>
2022-11-17 19:33:37 +0600

    updated 2

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,7 +1,7 @@
 import Product from "../../models/Product";
 import User from "../../models/User";
 import data from "../../utils/data";
-import db from "../../utils/DB.JS"
+import db from "../../utils/db"
 
 const handler = async (req,res)=>{
     await db.connect();
